8f740765e0c77fa692f7103fd23d4bf4bf7a952e
Roger Keithi <rogerkeithi@icloud.com>
2026-05-19 01:32:38 -0300

    feat: docker hub

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 async function bootstrap() {
   const { default: app } = await import("./app");
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
 
   app.listen(port, () => {
     console.log(`API running on port ${port}`);
