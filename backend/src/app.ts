2062325f572f3c911fef79799c75d1d438a78c62
Roger Keithi <rogerkeithi@icloud.com>
2026-06-02 19:23:02 -0300

    feat(user): find user by email

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,6 +8,13 @@ import { openApiDocument } from "./docs/open-api";
 import userRoute from "./interfaces/http/routers/user.route";
 
 const app = express();
+
+console.log(JSON.stringify(openApiDocument.paths, null, 2));
+
+console.log(
+  JSON.stringify(openApiDocument.paths?.["/users/by-email"], null, 2),
+);
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 app.use(
